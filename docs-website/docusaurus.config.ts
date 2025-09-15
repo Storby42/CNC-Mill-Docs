22aec58dbe3e61d48fb84c8b079d995582f39f69
Bean <70710230+Storby42@users.noreply.github.com>
2025-09-15 12:29:22 -0400

    Fixed GitHub link in TS config
    Now the "edit page" function actually opens the correct file on GitHub.

--- docs-website/docusaurus.config.ts
+++ docs-website/docusaurus.config.ts
@@ -45,7 +45,7 @@ const config: Config = {
           // Please change this to your repo.
           // Remove this to remove the "edit this page" links.
           editUrl:
-            'https://github.com/facebook/docusaurus/tree/main/packages/create-docusaurus/templates/shared/',
+            'https://github.com/Storby42/CNC-Mill-Docs/tree/main/docs-website/',
         },
         blog: {
           showReadingTime: true,
@@ -56,7 +56,7 @@ const config: Config = {
           // Please change this to your repo.
           // Remove this to remove the "edit this page" links.
           editUrl:
-            'https://github.com/facebook/docusaurus/tree/main/packages/create-docusaurus/templates/shared/',
+            'https://github.com/Storby42/CNC-Mill-Docs/tree/main/docs-website/',
           // Useful options to enforce blogging best practices
           onInlineTags: 'warn',
           onInlineAuthors: 'warn',
